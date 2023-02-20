28cd37583743922ed4029d8617987f9056df51bb
Sorted1 <87751266+Sorted1@users.noreply.github.com>
2023-02-20 04:14:26 -0500

    Update ipgeolocation.py

--- modules/ipgeolocation.py
+++ modules/ipgeolocation.py
@@ -14,15 +14,3 @@ def ipinfo(ip):
         city = res["city"]
         postal = res["postal"]
         print(f"IP: {ip}\nType: {typ}\nContinent: {cont}\nCountry: {cou}\nRegion: {region}\nCity: {city}\nPostal: {postal}")
-
-# Code for calling function - Dont run with this code below in file
-  
-# import ipgeolocation
-
-# ip_address = input("Enter an IPv4 address: ")
-# ip_info = ipgeolocation.get_ip_info(ip_address)
-
-# if ip_info is not None:
-#     print(f"{ip_info['ip_address']} - {ip_info['continent_name']} - {ip_info['state_prov']} - {ip_info['district']} - {ip_info['city']} - {ip_info['zipcode']} - {ip_info['asn']} Request Finished!")
-# else:
-#     print("Error: Failed to get IP information.")
