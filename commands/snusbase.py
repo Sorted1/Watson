ab299d31c5e7e3a10ac8d1ceaa91e78fdd5cf02a
Ian S <87751266+Sorted1@users.noreply.github.com>
2023-03-09 14:12:00 -0500

    Update snusbase.py

--- commands/snusbase.py
+++ commands/snusbase.py
@@ -4,8 +4,14 @@ with open("config.json") as f:
     config = json.load(f)
     key = config.get('beta.snusbase')
 
-print("""[1] Password Lookup\n[2] Hash Search""")
+print("""[1] Password Lookup\n[2] Hash Search\n[3] IP Information""")
 choice = input("Choice: ")
+
+def ipsearch():
+    ip = input("IP Address: ")
+    r = requests.get(f"https://beta.snusbase.com/v1/whois/{ip}", headers={"authorization":f"{key}"})
+    res = r.json()
+    print(f"As: {res['as']}\nCity: {res['city']}\nCountry: {res['country']}\nCountry Code: {res['countryCode']}\nISP: {res['isp']}\nMobile: {res['mobile']}\nOrganization: {res['org']}\nProxy: {res['proxy']}\nRegion: {res['region']}\nState: {res['regionName']}\nZip: {res['zip']}")
 
 def passwordsearch():
     password = input("Password: ")
@@ -22,5 +28,7 @@ if choice == "1":
     passwordsearch()
 elif choice == "2":
     hashsearch()
+elif choice == "3":
+    ipsearch()
 else:
     print("Invalid Choice")
